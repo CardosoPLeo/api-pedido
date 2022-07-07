141ed62b4afc328f57e777fa75db55a40246c6e3
Leonardo Peres Cardoso <leonardo.devops@alterdata.com.br>
2022-07-06 22:27:23 -0300

    Implementaação do método NotNull()

--- Pedidos.Dominio/Validations/ClienteValidation.cs
+++ Pedidos.Dominio/Validations/ClienteValidation.cs
@@ -9,14 +9,17 @@ namespace Pedidos.Dominio.Validations
         public ClienteValidation()
         {
             RuleFor(n => n.Nome)
+                .NotNull()
                 .NotEmpty()
                 .Length(3, 50);
             
             RuleFor(e => e.Email)
+                .NotNull()
                 .NotEmpty()
                 .EmailAddress();
 
             RuleFor(e => e.Enedereco)
+                .NotNull()
                 .NotEmpty();
         }
     }
