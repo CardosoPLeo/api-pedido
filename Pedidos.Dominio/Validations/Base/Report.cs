032c2d12f527acf27e9a0a5c9764accc6a908106
CardosoPLeo <leonardo_p_c@hotmail.com>
2022-07-07 22:40:08 -0300

    Criação de construtores

--- Pedidos.Dominio/Validations/Base/Report.cs
+++ Pedidos.Dominio/Validations/Base/Report.cs
@@ -4,5 +4,18 @@ namespace Pedidos.Dominio.Validations.Base
     {
         public string Codigo { get; set; }
         public string Mensagem { get; set; }
+
+        public Report()
+        {
+            
+        }
+
+        public Report(string mensagem)
+        {
+            Mensagem = mensagem;
+        }
+        
+        public static Report Create(string mensagem) => new Report(mensagem);
+        
     }
 }
